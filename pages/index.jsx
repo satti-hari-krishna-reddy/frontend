c067f580ca79e0316e66129da8ba6247e66b2b62
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2024-12-01 01:01:13 +0530

    remove unused imports

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,6 @@
 import Hero 		from '../components/sections/index/hero'
-import Looking 		from '../components/sections/index/looking'
 import About 		from '../components/sections/index/about'
 import Technical 	from '../components/sections/index/technical'
-import Career 		from '../components/sections/index/career'
-import FeaturedProjects	from '../components/sections/projects/featured'
-
 import Color 		from '../components/utils/page.colors.util'
 
 import colors 		from '../content/index/_colors.json'
@@ -16,11 +12,8 @@ export default function HomePage() {
 		<>
 			<Color colors={colors} />
 			<Hero />
-			{/* <Looking /> */}
-
 			<About />
 			<Technical />
-			{/* <Career /> */}
 		</>
 	);
 }
